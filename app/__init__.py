48ad3773223c799b08754087c5e5ebbf1df62362
Emory Walsh <ewalsh00@stuy.edu>
2020-04-27 14:45:06 -0400

    Added graphs route

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def map():
     return render_template("home.html")
+
+@app.route('/graphs')
+def graph():
+    return render_template("graphs.html")
 
 if __name__ == '__main__':
     app.debug = True
