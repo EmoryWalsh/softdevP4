6e52fdafe32bf23f5abf5a3ce59732c072d929c2
Emory Walsh <ewalsh00@stuy.edu>
2020-04-27 14:29:15 -0400

    Basic flask app running and displays map on render

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,13 @@
 import os
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-#DIR = os.path.dirname(__file__)
-#DIR += "/"
+@app.route('/')
+def hello():
+    return render_template("home.html")
 
-@app.route("/")
-def hello_world():
-	return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.debug = False
+if __name__ == '__main__':
+    app.debug = True
     app.run()
-
