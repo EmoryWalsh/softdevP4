68501aa2a6d2eff361ba40c925d1cb6f01243c05
Sophie Nichol <snichol00@stuy.edu>
2020-04-29 13:33:39 -0400

    update

---
+++ app/static/js/graphs.js
@@ -0,0 +1,20 @@
+var rgn = document.getElementById("regionDropdown");
+var ftr = document.getElementById("factorDropdown");
+
+function regAvg(reg, fac){
+  var i = 0
+  var total = 0
+  for country in app{
+    if app['region'] == reg:
+      total += app[reg]
+  }
+}
+
+function graphRegion(reg){
+}
+
+function graphFactor(fac){
+}
+
+rgn.addEventListener("click",graphRegion);
+ftr.addEventListener("click",graphFactor);
