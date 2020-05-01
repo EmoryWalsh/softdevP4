7d1d15133e76399c862b05b1ba7ef59aa950ea0f
Sophie Nichol <snichol00@stuy.edu>
2020-05-01 15:37:22 -0400

    update

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -21,9 +21,31 @@ function regAvg(reg, fac){
 console.log(regAvg('North America', 'freedom'));
 //console.log("hi");
 
-/*
-function graphRegion(reg){
 
+function graphRegion(reg){
+  var data = [
+  {
+    className: reg,
+    axes: [
+      {axis: "economy", value: regAvg(reg, 'economy')},
+      {axis: "family", value: regAvg(reg, 'family')},
+      {axis: "health", value: regAvg(reg, 'health')},
+      {axis: "freedom", value: regAvg(reg, 'freedom')},
+      {axis: "trust", value: regAvg(reg, 'trust')},
+      {axis: "generosity", value: regAvg(reg, 'generosity')},
+      {axis: "other", value: regAvg(reg, 'other')}
+    ]
+    },
+  ];
+  /*
+  return data.map(function(d) {
+    return {
+      className: d.className,
+      axes: d.axes.map(function(axis) {
+        return {axis: axis.axis, value: Math.ceil(Math.random() * 10)};
+      })
+    };
+  });*/
 }
 
 function graphFactor(fac){
