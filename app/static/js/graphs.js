b159340b85719bf95ff8f8a5eefe7ae865216c32
Emory Walsh <ewalsh00@stuy.edu>
2020-05-01 15:10:18 -0400

    Took out unessecary prints

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -8,18 +8,18 @@ function regAvg(reg, fac){
 //  console.log(data["Denmark"])
   for (country in data){
     if (data[country]['region'] == reg){
-      console.log(data[country][fac]);
+      //console.log(data[country][fac]);
       total += parseFloat(data[country][fac]); //need to fins a way to sub in fac
       num ++;
       //console.log(data[country])
     }
   }
-  console.log(total)
+  //console.log(total)
   return total / num;
 }
 
 console.log(regAvg('North America', 'freedom'));
-console.log("hi");
+//console.log("hi");
 /*
 function graphRegion(reg){
 
