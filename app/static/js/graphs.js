fb4cd979ad1979a698739763d70ed710d500c721
Sophie Nichol <snichol00@stuy.edu>
2020-05-05 21:48:35 -0400

    added dropdowns for bar r graphs

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -255,5 +255,54 @@ function getPathCoordinates(data_point, data){
     return coordinates;
 }
 
+factorBar = function(e){
+  // set the dimensions and margins of the graph
+  var margin = {top: 20, right: 20, bottom: 20, left: 20},
+      width = 400,
+      height = 400;
+
+  // set the ranges
+  var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+  var y = d3.scaleLinear()
+            .range([height, 0]);
+
+  // append the svg object to the body of the page
+  // append a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("#svg3").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  //get data
+
+    // Scale the range of the data in the domains
+    //x.domain(data.map(function(d) { return d.salesperson; }));
+    //y.domain([0, d3.max(data, function(d) { return d.sales; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.salesperson); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.sales); })
+        .attr("height", function(d) { return height - y(d.sales); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+}
+
 rReg.addEventListener("click",graphRegion);
 rFac.addEventListener("click",graphFactor);
